a96e59a879e622e17cfe78dd8d9f573bb2eeb0b5
Actimiel <73654261+Actimiel@users.noreply.github.com>
2020-11-10 19:04:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,14 @@
-test 
+//ici on précise qu'on aura besoin du module discord.js qui lui
+appelle l'API de Discord
+const Discord = require("discord.js");
+//ici on appelle une fonction de l'objet créé au-dessus qui permet
+de récupérer un objet utilisateur client qui représente le bot
+const bot = new Discord.Client();
+bot.on("ready", function () {
+ //ici on affichera dans le terminal que le bot est bien
+connecté
+ console.log("Carapuce est dans les places !");
+});
+//la ligne suivante permet d'indiquer à l'objet Discord qui est
+notre bot afin qu'il puisse se connecter
+bot.login("Nzc1NzgwNzg3NDc0OTIzNTIw.X6rUKQ.V0UUh3piq9CT7Fs_d7Y1rrVVeco");
